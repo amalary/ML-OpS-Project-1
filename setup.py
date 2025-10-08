4164e8006a2cadfd2ed3f943047e1d3bfaf07e6a
amalary <amalary@bu.edu>
2025-10-08 16:34:05 -0700

    Commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f: 
+
+   requirements = f.read().splitlines()
+
+setup(
+   
+   name = "MLOPS-Project-1", 
+   version= "0.1",
+   author = "AnthonyM", 
+   packages = find_packages(),
+   install_requires = requirements, 
+)
